bfc169deafb4486c8849e4ae06e9251c7f547778
Semyon Surkov <semyonsurkov@outlook.com>
2024-02-28 15:38:48 +0300

    Add -host option for vite and fix styles into ProductList

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+const hosts = ['localhost', '10.8.0.2', '192.168.100.10'];
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    host: hosts.join(','),
+  },
+});
